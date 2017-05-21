80095f2b9a291329d6241097e0d65c9fbe75a398
HPierce <hpierce1102@gmail.com>
2017-05-21 17:55:15 -0500

    Create test harness

---
+++ src/Problem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class Problem {
+    public function __construct()
+    {
+
+    }
+}
---
+++ tests/ProblemTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests;
+
+
+use App\Problem;
+use PHPUnit\Framework\TestCase;
+
+
+class ProblemTest extends TestCase
+{
+    public static function setUpBeforeClass()
+    {
+
+    }
+
+    public function setUp(){
+
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+    public static function tearDownAfterClass()
+    {
+
+    }
+}
